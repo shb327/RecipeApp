7fc2532588061495ca50d9ef14feba64492fb678
denisdnepr66 <denisdenpr66@gmail.com>
2021-11-02 07:48:07 +0100

    secured api key

--- app/src/main/java/com/example/recipeapptest2/network/RecipeService.kt
+++ app/src/main/java/com/example/recipeapptest2/network/RecipeService.kt
@@ -1,11 +1,14 @@
 package com.example.recipeapptest2.network
 
+import com.example.recipeapptest2.BuildConfig
 import com.example.recipeapptest2.model.Recipes
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface RecipeService {
 
-    @GET("recipes/complexSearch?apiKey=068e1323345442f3a4621a2aebd2919b")
+    @GET("recipes/complexSearch?apiKey=${BuildConfig.SPC_KEY}")
     fun getRecipe(): Single<Recipes>
+
+
 }
